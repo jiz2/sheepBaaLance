532193ca33744f344b0ef1d8787c385a99da34a2
Jianfei Zheng <jiz2@hi.is>
2018-10-20 13:25:26 +0000

    Added my own Scene, tested out Relative Joint instead of Hinge Joint. Improved Background objects.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D player;
+
+	public float speed;
+	public float muliplier = 1;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        // Store the current horizontal input in the float moveHorizontal.
+        // float moveHorizontal = Input.acceleration.x;
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        player.AddForce(movement * speed * muliplier);
+
+		// Doesn't work since it does not recompute the Relative Joint
+        // transform.Translate(moveHorizontal * speed * muliplier * Time.deltaTime, 0, 0);
+	}
+}
---
+++ Assets/Sprites/SheepAI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepAI : MonoBehaviour {
+
+	private float lifeTime = 0f;		// Total seconds elapsed since Sheep has been in the game
+
+	public float MadnessTimer;			// The threshold in seconds when the sheep starts going crazy
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lifeTime += Time.deltaTime;
+	}
+}
